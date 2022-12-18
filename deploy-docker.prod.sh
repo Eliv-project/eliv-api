14b4fd851f2f19c0fad571f7c2acf9b80031f005
dellusia <dnntung@gmail.com>
2022-12-18 15:46:32 +0700

    fix: Update deploy script

--- deploy-docker.prod.sh
+++ deploy-docker.prod.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-docker-compose --env-file ./api/.env.docker -f ./docker-compose.prod.yml down
-docker-compose --env-file ./api/.env.docker -f ./docker-compose.prod.yml up --build -d
-docker image prune -f
+docker compose --env-file ./api/.env.docker -f ./docker-compose.prod.yml down
+docker compose --env-file ./api/.env.docker -f ./docker-compose.prod.yml up --build -d
